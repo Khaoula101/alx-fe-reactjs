89f5fd5beed7b75f3afd93165224b1809dc7db4c
Khaoula101 <badrisabdelilah@badriss-MacBook-Pro.local>
2024-08-18 20:29:44 +0100

    add counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,12 @@
+import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
+    </>
   );
 }
 
