1de3d9ba2ef7d25680991c2c7c29563c3d97f9b0
Khaoula101 <badrisabdelilah@badriss-MacBook-Pro.local>
2024-08-18 21:00:18 +0100

    add my-company app with routing

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "#333",
+        color: "#fff",
+      }}
+    >
+      <p>&copy; 2024 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
