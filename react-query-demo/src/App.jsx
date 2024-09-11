39ee32cd92c50ea922fbab004baff0735068e794
Khaoula101 <badrisabdelilah@badriss-MBP.home>
2024-09-11 11:15:40 +0100

    move posts component to components folder

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import PostsComponent from "./PostsComponent";
+import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
 
