e0c07327202abab962a0b81c3afba41e8e4b1930
Khaoula101 <badrisabdelilah@badriss-MBP.home>
2024-09-11 12:13:42 +0100

    add todo

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,34 @@
+// src/__tests__/TodoList.test.js
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import TodoList from "../TodoList";
+
+test("renders TodoList component", () => {
+  render(<TodoList />);
+  expect(screen.getByText(/Todo List/i)).toBeInTheDocument();
+  expect(screen.getByPlaceholderText(/Add new todo/i)).toBeInTheDocument();
+});
+
+test("adds a new todo item", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText(/Add new todo/i), {
+    target: { value: "New Todo" },
+  });
+  fireEvent.click(screen.getByText(/Add/i));
+  expect(screen.getByText("New Todo")).toBeInTheDocument();
+});
+
+test("toggles a todo item", () => {
+  render(<TodoList />);
+  const todoItem = screen.getByText("Learn React");
+  fireEvent.click(todoItem);
+  expect(todoItem).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo item", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getAllByText(/Delete/i)[0];
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
