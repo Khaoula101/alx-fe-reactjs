c88311696a1c0e436182c314c6590fa441fcadd7
Khaoula101 <badrisabdelilah@badriss-MBP.home>
2024-09-11 11:30:05 +0100

    add blog route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,8 +1,8 @@
 import { useParams } from "react-router-dom";
 
 function BlogPost() {
-  const { postId } = useParams();
-  return <div>Now showing blog post {postId}</div>;
+  const { id } = useParams();
+  return <div>Now showing blog post {id}</div>;
 }
 
 export default BlogPost;
