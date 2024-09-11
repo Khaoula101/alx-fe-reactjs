ce09fb6f5f4274a83867aafa15d600e9c21e6958
Khaoula101 <badrisabdelilah@badriss-MBP.home>
2024-09-11 11:31:22 +0100

    add protected route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,11 +3,20 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 function App() {
+  const isAuthenticated = true;
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
