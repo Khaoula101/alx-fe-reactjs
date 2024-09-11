ec0b7f0a54173be1bd6b89bd88d9019f469b17ab
Khaoula101 <badrisabdelilah@badriss-MBP.home>
2024-09-11 11:14:28 +0100

    add react query

---
+++ react-query-demo/src/PostsComponent.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query";
+
+// Fetch function
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+function PostsComponent() {
+  // Use the useQuery hook to fetch posts
+  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching posts!</p>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
