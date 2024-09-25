e51ac0d1507d62b3e5549ce9d0526f7629fd0894
Khaoula101 <badrisabdelilah@192.168.1.77>
2024-09-25 18:07:33 +0100

    init github project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      {/* Search Component and Result Components will go here */}
+    </div>
+  );
+}
+
+export default App;
